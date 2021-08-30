d50d62fb43d815a9ee57b1fcc7cf3c227a9f430b
Chris Nelson <chris@gaslight.co>
2021-08-30 16:10:30 -0400

    prepare for publication

--- lib/test_with_retry_task.ex
+++ lib/test_with_retry_task.ex
@@ -1,10 +1,18 @@
 defmodule Mix.Tasks.TestWithRetry do
   @preferred_cli_env "test"
 
+  @moduledoc """
+  A mix task to retry failed tests. It accepts the same command line
+  arguments as `mix test`. If failures are encountered on the first test run, it will do another
+  run adding the `--failed` option to only run failed tests. If tests continue to fail, it will
+  return an exit code of 1 so that CI can report failures appropriately.
+  """
+
   def run(args) do
     case System.cmd("mix", ["test"] ++ args, into: IO.stream(:stdio, :line)) do
       {_output, 0} ->
         nil
+
       {_, _} ->
         IO.puts("Rerunning failed tests...")
         retry(args)
@@ -15,6 +23,7 @@ defmodule Mix.Tasks.TestWithRetry do
     case System.cmd("mix", ["test", "--failed"] ++ args, into: IO.stream(:stdio, :line)) do
       {_output, 0} ->
         nil
+
       {_, _} ->
         exit(1)
     end
--- mix.exs
+++ mix.exs
@@ -4,10 +4,17 @@ defmodule TestWithRetry.MixProject do
   def project do
     [
       app: :test_with_retry,
+      description: "A mix task to automatically retry any failed tests. Takes same args as mix test. Reports failures on second run.",
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        licenses: ["MIT"],
+        links: %{"Github" => "https://github.com/gaslight/test_with_retry"}
+      ],
+      source_url: "https://github.com/gaslight/test_with_retry",
+      homepage_ur: "https://github.com/gaslight/test_with_retry"
     ]
   end
 
@@ -21,8 +28,7 @@ defmodule TestWithRetry.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
